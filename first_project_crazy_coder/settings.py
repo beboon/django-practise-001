03abe3c2ab104e2635c04f3e72d7ee9c84e5c2d0
Beboon <38987483+beboon@users.noreply.github.com>
2022-04-22 17:12:44 +0600

    Update settings.py
    Fix security issue.

--- first_project_crazy_coder/settings.py
+++ first_project_crazy_coder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')y$-pnt#g2n5ye3j4$n_2w6g2_@$78ade$6-wp^e1xl#tq#69&'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
